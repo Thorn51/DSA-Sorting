54c1112a85064cd0976c025623aafb15cb7d010e
Matt Thornberry <mtberry16@hotmail.com>
2020-03-10 17:09:51 -0600

    add quick sort

--- main.js
+++ main.js
@@ -1,4 +1,124 @@
-let testArray = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40];
+let testArrayOne = [
+  21,
+  1,
+  26,
+  45,
+  29,
+  28,
+  2,
+  9,
+  16,
+  49,
+  39,
+  27,
+  43,
+  34,
+  46,
+  40
+];
+let testArrayTwo = [14, 17, 13, 15, 19, 10, 3, 16, 9, 12];
+let testArrayThree = [
+  89,
+  30,
+  25,
+  32,
+  72,
+  70,
+  51,
+  42,
+  25,
+  24,
+  53,
+  55,
+  78,
+  50,
+  13,
+  40,
+  48,
+  32,
+  26,
+  2,
+  14,
+  33,
+  45,
+  72,
+  56,
+  44,
+  21,
+  88,
+  27,
+  68,
+  15,
+  62,
+  93,
+  98,
+  73,
+  28,
+  16,
+  46,
+  87,
+  28,
+  65,
+  38,
+  67,
+  16,
+  85,
+  63,
+  23,
+  69,
+  64,
+  91,
+  9,
+  70,
+  81,
+  27,
+  97,
+  82,
+  6,
+  88,
+  3,
+  7,
+  46,
+  13,
+  11,
+  64,
+  76,
+  31,
+  26,
+  38,
+  28,
+  13,
+  17,
+  69,
+  90,
+  1,
+  6,
+  7,
+  64,
+  43,
+  9,
+  73,
+  80,
+  98,
+  46,
+  27,
+  22,
+  87,
+  49,
+  83,
+  6,
+  39,
+  42,
+  51,
+  54,
+  84,
+  34,
+  53,
+  78,
+  40,
+  14,
+  5
+];
 
 // 1. Understanding merge sort
 
@@ -40,8 +160,42 @@ function merge(left, right, array) {
   return array;
 }
 
-console.log(testArray);
+// 2. Understanding quicksort
 
-let sortedArray = mergeSort(testArray);
+function quickSort(array, start = 0, end = array.length) {
+  // Nothing to sort here
+  if (start >= end) {
+    return array;
+  }
 
-console.log(testArray);
+  const middle = partition(array, start, end);
+  array = quickSort(array, start, middle);
+  array = quickSort(array, middle + 1, end);
+
+  return array;
+}
+
+//Lomuto's partition algorithm
+
+function partition(array, start, end) {
+  const pivot = array[end - 1];
+  let j = start;
+  for (let i = start; i < end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
+  }
+  swap(array, end - 1, j);
+  return j;
+}
+
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+}
+
+let testQuickSort = quickSort(testArrayOne);
+
+console.log(testQuickSort);
