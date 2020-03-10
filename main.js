6dd31001b255dbbfb183e88d50ebb685d998301a
Matt Thornberry <mtberry16@hotmail.com>
2020-03-10 16:37:16 -0600

    add merge sort

---
+++ main.js
@@ -0,0 +1,47 @@
+let testArray = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40];
+
+// 1. Understanding merge sort
+
+function mergeSort(array) {
+  // Nothing to sort here
+  if (array.length <= 1) {
+    return array;
+  }
+
+  debugger;
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+  return merge(left, right, array);
+}
+
+function merge(left, right, array) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    } else {
+      array[outputIndex++] = right[rightIndex++];
+    }
+  }
+
+  for (let i = leftIndex; i < left.length; i++) {
+    array[outputIndex++] = left[i];
+  }
+
+  for (let i = rightIndex; i < right.length; i++) {
+    array[outputIndex++] = right[i];
+  }
+  return array;
+}
+
+console.log(testArray);
+
+let sortedArray = mergeSort(testArray);
+
+console.log(testArray);
